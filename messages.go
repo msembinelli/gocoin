5672fd15873ceb636ed7679ea3afc6953a2e9ed0
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2017-07-02 00:35:11 -0600

    Added serialization for version and net_addr
    Also seed random number generator with UnixNano

--- messages.go
+++ messages.go
@@ -5,11 +5,9 @@ import (
     "time"
     "net"
     "math/rand"
+    "encoding/binary"
+    "bytes"
     )
-// import(
-//     "encoding/binary"
-//     "bytes"
-// )
 
 var PROTOCOL_VERSION int32 = 70015
 var CADDR_TIME_VERSION uint32 = 31402
@@ -18,15 +16,25 @@ var MAINNET_TCP_PORT uint16 = 8333 // bitcoin main network port
 var TESTNET_MAGIC uint32 = 0x0B110907 // bitcoin test network
 var TESTNET_TCP_PORT uint16 = 18333 // bitcoin main network port
 
-// TODO: Add serialization function
 type net_addr struct {
     time uint32
     services uint64
-    ip []byte
+    ip [16]byte
     port uint16
 }
 
-// TODO: Add serialization function
+func (n net_addr) serialize() *bytes.Buffer {
+    buffer := new(bytes.Buffer)
+
+    err := binary.Write(buffer, binary.BigEndian, n)
+    if err != nil {
+        fmt.Println(err)
+        return new(bytes.Buffer)
+    }
+
+    return buffer
+}
+
 type version_msg struct {
     version int32
     services uint64
@@ -34,12 +42,27 @@ type version_msg struct {
     addr_recv net_addr
     addr_from net_addr
     nonce uint64
-    user_agent string
+    user_agent [1]byte
     start_height int32
     relay bool
 }
 
+func (v version_msg) serialize() *bytes.Buffer {
+    buffer := new(bytes.Buffer)
+
+    err := binary.Write(buffer, binary.BigEndian, v)
+    if err != nil {
+        fmt.Println(err)
+        return new(bytes.Buffer)
+    }
+    fmt.Println(buffer.Bytes())
+    return buffer
+}
+
 func main() {
+    rand.Seed(time.Now().UnixNano())
+
+    // Create version message
     var v version_msg
     v.version = PROTOCOL_VERSION
     v.services = 1
@@ -50,33 +73,16 @@ func main() {
     r.time = CADDR_TIME_VERSION
     r.services = 1
     ip := net.IP([]byte{127, 0, 0, 1})
-    fmt.Println(ip.To16())
-    r.ip = ip.To16()
+    copy(r.ip[:], ip.To16())
     r.port = MAINNET_TCP_PORT
     v.addr_recv = r
     v.addr_from = r
 
     v.nonce = rand.Uint64()
-    v.user_agent = ""
+    copy(v.user_agent[:], "")
     v.start_height = 0
     v.relay = false
 
-    // TODO: fix up and move to serialization functions for structs above
-    // ip := binary.BigEndian.Uint32(net.IPv4(127, 0, 0, 1))
-    // var port uint32 = 8333
-    // buf := new(bytes.Buffer)
-    // for i := 0; i < 2; i++ {
-    //     // pack addr_me and addr_you
-    //     err := binary.Write(buf, binary.BigEndian, ip)
-    //     if err != nil {
-    //      fmt.Println(err)
-    //      return
-    //     }
-    //     buf_port := new(bytes.Buffer)
-    //     err = binary.Write(buf, binary.BigEndian, port)
-    //     if err != nil {
-    //      fmt.Println(err)
-    //      return
-    //     }
-    // }
+    fmt.Println(v.serialize())
+
 }
